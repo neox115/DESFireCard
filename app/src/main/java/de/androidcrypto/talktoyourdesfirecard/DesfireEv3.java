5d75c4d768ab7ed9cf3ea6a9d6a9bf3c6302fd46
neox115 <neox22neox@akane.waseda.jp>
2025-12-29 02:17:12 +0900

    Simplify command string by removing unused bytes
    Removed unnecessary ISO options and DF name length bytes from command string construction.

--- app/src/main/java/de/androidcrypto/talktoyourdesfirecard/DesfireEv3.java
+++ app/src/main/java/de/androidcrypto/talktoyourdesfirecard/DesfireEv3.java
@@ -952,13 +952,9 @@ public class DesfireEv3 {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         baos.write(fileNumber);
         baos.write(isoFileId, 0, isoFileId.length);
-        baos.write((byte)0x00); // ISO options
-        baos.write((byte)0x00); // ISO DF name length (EV3)
         baos.write(commSettings);
         baos.write(accessRights, 0, accessRights.length);
         baos.write(fileSizeByte, 0, fileSizeByte.length);
-        baos.write(0x00);
-        baos.write(0x00);
         byte[] commandParameter = baos.toByteArray();
         byte[] apdu;
         byte[] response;
