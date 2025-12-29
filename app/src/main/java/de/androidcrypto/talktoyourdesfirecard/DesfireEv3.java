bed54963592d724562bda93044aeecd741584e4a
neox115 <neox22neox@akane.waseda.jp>
2025-12-29 23:23:32 +0900

    Refactor wrapMessageNoLe to remove unnecessary bytes
    Removed unused bytes from wrapMessageNoLe method.

--- app/src/main/java/de/androidcrypto/talktoyourdesfirecard/DesfireEv3.java
+++ app/src/main/java/de/androidcrypto/talktoyourdesfirecard/DesfireEv3.java
@@ -10764,8 +10764,6 @@ fileSize: 128
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         stream.write((byte) 0x90);
         stream.write(command);
-        stream.write((byte) 0x00);
-        stream.write((byte) 0x00);
         if (parameters != null) {
             stream.write((byte) parameters.length);
             stream.write(parameters);
